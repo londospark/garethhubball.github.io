78bf6f86c2f8f2ac58082d75d8ac52cad99f58ba
Gareth Hubball <ridecar2@gmail.com>
2018-10-27 01:14:15 +0100

    Changed theme

--- publish.bat
+++ publish.bat
@@ -3,6 +3,9 @@
 call .paket\paket restore
 call packages\FSharp.Formatting.CommandTool\tools\fsformatting.exe literate --processDirectory --lineNumbers true --inputDirectory  "code" --outputDirectory "_posts"
 
+call dotnet build .\filefixer\filefixer.fsproj
+call dotnet run --project .\filefixer\filefixer.fsproj _posts
+
 git add --all .
 git commit -a -m %1
 git push
